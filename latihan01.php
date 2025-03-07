b58a7b2bd02d38851fa4afb7bdb51f6d4f404492
zalZEEY <moyok7889@gmail.com>
2025-03-07 22:01:04 +0700

    first commit

---
+++ latihan01.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo "Belajar PHP" ?></title>
+</head>
+<body>
+    <?php
+        $nama = "Rizal";
+        $umur = 17;
+        echo "saya sedang belajar php<br>";
+        echo "<p>Belajar PHP hingga jadi master</p>";
+        echo "Umur saya adalah umur $umur"
+    ?>
+</body>
+</html>
---
+++ modul1php/modul1variable.php
@@ -0,0 +1,16 @@
+<?php
+$angka=128;
+$huruf="Tes Variabel";
+$desimal=0.5;
+
+echo $angka;
+echo '<br>';
+echo $huruf;
+echo '<br>';
+echo $desimal;
+echo '<br>';
+echo $angka.' - '.$huruf.' - '.$desimal;
+echo '<br>';
+echo $angka*$desimal;
+echo '<br><br><i>Coded by Rizal Bagus Putra Agusta</i>';
+?>
