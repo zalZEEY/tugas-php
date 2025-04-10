d1f10b8154f7c783899cee947ef097a882dd5b81
zalZEEY <moyok7889@gmail.com>
2025-04-10 11:00:37 +0700

    1 - 8

--- modul2php/library.php
+++ modul2php/library.php
@@ -3,4 +3,15 @@ function CetakHTML($tag, $text)
 {
     echo "<$tag>$text</$tag>";
 }
+function GetList($nama, $jml, $harga)
+{
+    $total = $jml * $harga;
+    $list="<tr>
+            <td>$nama</td>
+            <td>$jml</td>
+            <td>$harga</td>
+            <td>$total</td>
+          </tr>";
+    return $list;
+}
 ?>
---
+++ modul2php/variable.php
@@ -0,0 +1,7 @@
+<?php
+$barang = array(
+    array('Nabati', 3, 10000),
+    array('Buavita', 5, 28000),
+    array('Regal', 8, 12000),
+);
+?>
